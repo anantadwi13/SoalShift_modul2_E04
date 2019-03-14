7cb6634bbf8974386bae308ee430ba2a5173dabb
Patrick Sungkharisma <36809780+ArisatoX@users.noreply.github.com>
2019-03-14 23:16:57 +0700

    soal4 btuh dicek

--- soal4/soal4.c
+++ soal4/soal4.c
@@ -62,7 +62,7 @@ int main() {
 	}
 	*/
 
-    stat("/home/anantadwi13/Documents/makanan/makan_enak.txt", &details);
+    stat("/home//Documents/makanan/makan_enak.txt", &details);
 
     char *waktu2 = ctime(&details.st_atime);
 	//printf("\n%s", waktu2);
@@ -79,28 +79,24 @@ int main() {
 	}
 	*/
 
-    int daydif,dif0;
+    int daydif;
     daydif = ((temp1[0]-'0')*10) + (temp1[1]-'0') - ((temp2[0]-'0')*10) - (temp2[1]-'0');
 	//printf("\n%d\n",daydif);
-    if (daydif != 0)
-    {
-       dif0= daydif * 24 * 3600;
-       dif1=((temp1[3]-'0')*10) + (temp1[4]-'0') + dif0 - ((temp2[3]-'0')*10) - (temp2[4]-'0');
-    }
-    else dif1=((temp1[3]-'0')*10) + (temp1[4]-'0') - ((temp2[3]-'0')*10) - (temp2[4]-'0');
-
+    
+    dif1=((temp1[3]-'0')*10) + (temp1[4]-'0') - ((temp2[3]-'0')*10) - (temp2[4]-'0');
     dif2=((temp1[6]-'0')*10) + (temp1[7]-'0') - ((temp2[6]-'0')*10) - (temp2[7]-'0');
     dif3=((temp1[9]-'0')*10) + (temp1[10]-'0') - ((temp2[9]-'0')*10) - (temp2[10]-'0');
 	//printf("%d %d %d",dif1, dif2, dif3);
+
     int second = dif1*3600 + dif2*60 + dif3;
-    if (second <= 30)
+    if (daydif == 0 && second <= 30)
     {
-      char temp3[200];
+      char temp3[50];
       counter++;
       sprintf(temp3,"/home/arisatox/Documents/makanan/makan_sehat%d.txt",counter);
-      FILE *fPoint;
-      fPoint = fopen(temp3, "a");
-      fclose(fPoint);
+      FILE *fp;
+      fp = fopen(temp3, "a");
+      fclose(fp);
     }
     sleep(5);
   }
