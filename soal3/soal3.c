dc542c3a10b26eb634ad1480c6694afec334d48d
Ananta Dwi Prasetya Purna Yuda <anantadwi13@yahoo.com>
2019-03-14 17:43:25 +0700

    passing argument

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -3,12 +3,18 @@
 #include <unistd.h>
 #include<stdio.h>
 
-int main()
+int main(int argc, char **args)
 {
    pid_t child_id;
    pid_t grandchild_id;
    int statuschild;
    int statusgrandchild;
+
+    if (argc<2) {
+        printf("Usages :\n%s /path/to/file/campur2.zip\n", args[0]);
+        return 0;
+    }
+    
 
    child_id = fork();
 
@@ -18,7 +24,7 @@ int main()
 
       if (grandchild_id == 0) 
       {
-            char *argv[4] = {"unzip","campur2.zip",NULL};
+            char *argv[4] = {"unzip", "-oq",args[1],NULL};
             execv("/usr/bin/unzip", argv);
       }
       else
